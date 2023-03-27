884f9fe7ecef9fa63a57149546e9b38276aa4594
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-27 11:06:15 +0200

    TP Finished

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-// Créer un événement au scroll
+let popupVisible = true;
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+window.addEventListener("scroll", () => {
+    if(window.scrollY == 0)
+    {
+        navbar.style.height = "90px";
+    }
+    else
+    {
+        navbar.style.height = "60px";
+    }
 
-// Faire apparaitre l'image de la partie improvise
+    if(window.scrollY >= 200)
+    {
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = "translateX(0)";
+    }
+    else
+    {
+        imgImprovise.style.opacity = 0;
+        imgImprovise.style.transform = "translateX(-200px)";
+    }
 
-// Faire apparaitre la popup quand on est en bas du site
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight && popupVisible)
+    {
+        popup.style.opacity = 1;
+        popup.style.transform = "translateX(0)";
+    }
+});
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+closeBtn.addEventListener("click", () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(400px)";
+    popupVisible = false;
+});
+
